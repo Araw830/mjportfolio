83cc70dc2f51b6ee6d4c41613ca0d93767eb1ef0
atulr830 <atulr830@gmail.com>
2021-07-15 07:35:18 +0530

    initial commit

---
+++ src/components/ContactButton.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ContactButton() {
+    return (
+        <div>
+            <div class="grid grid-cols-3 gap-4">
+  <div>1</div>
+  
+  <div>9</div>
+</div>
+            
+        </div>
+    )
+}
+
+export default ContactButton
---
+++ src/components/Content.js
@@ -0,0 +1,65 @@
+import React,{ useEffect,useRef } from 'react';
+import lottie from 'lottie-web'; 
+import { SocialIcon } from 'react-social-icons';
+
+function Content(){
+    const container=useRef(null);
+    useEffect(()=>{
+        lottie.loadAnimation({
+            container:container.current,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: require('../coder.json'),
+            rendererSettings: {
+                preserveAspectRatio: "xMidYMid slice"
+              }
+        });
+        return () => {
+            lottie.destroy();
+          };
+    },[])
+    return (
+
+        <div>
+            <div className="flex justify-evenly items-between flex-col sm:flex-row">
+                <div className="lg:mt-20 mt-14 ">
+                <h1 className="lg:text-justify lg:text-7xl font-extrabold text-4xl font-bold">Hi all, I'm Mayank <br/>Joshi 👋</h1>
+      <p className="lg:text-justify font-bold lg:text-2xl text-gray-400 mt-2">A passionate Android developer 🚀 and Iam  <br/> a Superman having good knowledge of Java, Kotlin,python  <br/> with JavaScript / Reactjs frameworks.</p>
+      <div className="grid grid-cols-4 lg:justify-items-start gap-x-2 gap-y-4 mt-2">
+          <div > <SocialIcon url="https://github.com/mayank" />   </div>
+          <div> <SocialIcon url="https://linkedin.com/mayank" />  </div>
+          <div> <SocialIcon url="https://twitter.com/mayank" />   </div>
+          <div><SocialIcon url="https://youtube.com/mayank" /></div>
+          
+         
+          </div>
+          <div className="lg:text-justify mt-4">
+          <button class="bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg uppercase">
+  Contact me
+</button>
+<button class="bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg ml-4 uppercase">
+  see my resume
+</button>
+
+              </div>
+
+                </div>
+                <div className="mt-10 h-200 w-200" ref={container}></div>
+            </div>
+
+
+
+         
+        </div>
+
+     
+
+
+
+
+
+    )
+}
+
+export default Content;
